4ab803f3be8857e9559fba6864db515d00ed8a1c
ishwar chaiser <ishwarchaiser@gmail.com>
2023-12-31 14:51:41 +0530

    component added to reacat app

---
+++ src/MyComponent/Todos.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { TodoItem } from "./TodoItem";
+
+export const Todos = () => {
+  return (
+    <div>
+      <h3>Todos</h3>
+      <TodoItem />
+    </div>
+  );
+};
