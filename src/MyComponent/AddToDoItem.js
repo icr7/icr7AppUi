78150e01d236a53ee1c15ad99e1b6309c360dc59
ishwar chaiser <ishwarchaiser@gmail.com>
2023-12-31 15:21:18 +0530

    add todos

---
+++ src/MyComponent/AddToDoItem.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+export const AddToDoItem = () => {
+  return (
+    <div>
+      <form className="row row-cols-lg-auto g-3 align-items-center">
+        <div className="col-12">
+          <label className="visually-hidden" for="inlineFormInputGroupUsername">
+            Username
+          </label>
+          <div className="input-group">
+            <div className="input-group-text">todo</div>
+            <input
+              type="text"
+              className="form-control"
+              id="inlineFormInputGroupUsername"
+              placeholder="Task name"
+            />
+          </div>
+        </div>
+
+        <div className="col-12">
+          <label className="visually-hidden" for="inlineFormSelectPref">
+            Preference
+          </label>
+          <select className="form-select" id="inlineFormSelectPref">
+            <option selected>Priority</option>
+            <option value="1">High</option>
+            <option value="2">Medium</option>
+            <option value="3">Low</option>
+          </select>
+        </div>
+
+        <div className="col-12">
+          <button type="submit" className="btn btn-primary">
+            Add
+          </button>
+        </div>
+      </form>
+    </div>
+  );
+};
--- src/MyComponent/Todos.js
+++ src/MyComponent/Todos.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { TodoItem } from "./TodoItem";
+import { AddToDoItem } from "./AddToDoItem";
 
 export const Todos = () => {
   return (
     <div>
       <h3>Todos</h3>
+      <AddToDoItem />
       <TodoItem />
     </div>
   );
