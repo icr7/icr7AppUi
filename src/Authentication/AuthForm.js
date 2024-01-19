78bb403a65a49fe33c50702a83a274e94d37f590
ishwar chaiser <ishwarchaiser@gmail.com>
2024-01-20 04:47:50 +0530

    added logo of icr7

--- src/Authentication/AuthForm.js
+++ src/Authentication/AuthForm.js
@@ -20,9 +20,9 @@ export const AuthForm = ({ onAuthFormSubmit, isSignUp }) => {
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <img
-            className="mx-auto h-10 w-auto"
-            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-            alt="Your Company"
+            className="mx-auto h-20 w-auto"
+            src="./icr7_logo.png"
+            alt="icr7 logo"
           />
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
             {isSignUp ? "Sign Up" : "Login"} to your account
